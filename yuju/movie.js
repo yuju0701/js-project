435618d6aa1191c7f741156bb265b343a88aa531
Yongmin0423 <yym21345@gmail.com>
2024-07-21 16:33:01 +0900

    home button

--- yuju/movie.js
+++ yuju/movie.js
@@ -239,6 +239,9 @@ document.getElementById("close-icon").addEventListener("click", function () {
 });
 
 // house 버튼 클릭 시 페이지 새로고침
+document.querySelector("#house-icon").addEventListener("click", function () {
+  location.reload();
+});
 
 // 상세페이지 열기
 const openDetailPage = (movieID) => {
